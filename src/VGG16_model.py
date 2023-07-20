b88163d8ffe8345d3cc1dec77e4f65977b735740
MichalMichniak <michalmichniak2001@gmail.com>
2023-07-20 13:47:08 +0200

    cuda check

---
+++ src/VGG16_model.py
@@ -0,0 +1,11 @@
+import torch
+
+class VGG16_model:
+    """
+    VGG16 architecture without cuda
+    """
+    def __init__(self) -> None:
+        ### input 32 x 32
+        ### 1 st visual geometry group
+        convolution_1 = 0
+        pass
