645ac544bbe70486d565e867eccc4a68f87967d0
MichalMichniak <michalmichniak2001@gmail.com>
2024-01-08 19:22:11 +0100

    update cleaning

--- faith_all.py
+++ faith_all.py
@@ -107,40 +107,40 @@ if __name__ == '__main__':
         data = fmeasure.get_all_same_sl_map(tr_fc=threshold_fc,perturbation_fc=eurosat_perturbation_inverted)
         data.to_csv(f"finished/ResNet50_new/faithfulness_metrics_grad_cam_plus_plus_inverted_noise_{name}.csv",index=False)
     print("combained ++")
-    # for noise,name in zip([0.5,1.0,1.5],["0_5","1","1_5"]):
-    #     def threshold_fc(silency_map):
-    #             return thr_fc(silency_map,scale=noise)
-    #     # model load:
-    #     resnet50 = torch.load("finished\\ResNet50_new\\resnet50_model.pth")
-    #     resnet50.cuda()
+    for noise,name in zip([0.5,1.0,1.5],["0_5","1","1_5"]):
+        def threshold_fc(silency_map):
+                return thr_fc(silency_map,scale=noise)
+        # model load:
+        resnet50 = torch.load("finished\\ResNet50_new\\resnet50_model.pth")
+        resnet50.cuda()
 
-    #     # add softmax
+        # add softmax
 
-    #     fc_ = list(resnet50.fc)
-    #     fc_.append(torch.nn.Softmax(dim=1))
-    #     resnet50.fc = torch.nn.Sequential(*fc_)
-    #     resnet50.eval()
+        fc_ = list(resnet50.fc)
+        fc_.append(torch.nn.Softmax(dim=1))
+        resnet50.fc = torch.nn.Sequential(*fc_)
+        resnet50.eval()
 
-    #     resnet50_plus = torch.load("finished\\ResNet50_new\\resnet50_model.pth")
-    #     resnet50_plus.cuda()
-    #     resnet50_plus.eval()
+        resnet50_plus = torch.load("finished\\ResNet50_new\\resnet50_model.pth")
+        resnet50_plus.cuda()
+        resnet50_plus.eval()
 
-    #     # dataset:
-    #     transforms = v2.Compose([
-    #         v2.ToTensor(),
-    #         v2.ToDtype(torch.float32),
-    #         v2.Resize(224,antialias=None),
-    #     ])
-    #     ds = EuroSAT("../EuroSat",transform=transforms,target_transform=transformation_eurosat,download=False)
-    #     ds_test = Test_Dataset_EuroSat(ds)
+        # dataset:
+        transforms = v2.Compose([
+            v2.ToTensor(),
+            v2.ToDtype(torch.float32),
+            v2.Resize(224,antialias=None),
+        ])
+        ds = EuroSAT("../EuroSat",transform=transforms,target_transform=transformation_eurosat,download=False)
+        ds_test = Test_Dataset_EuroSat(ds)
 
-    #     # target layer:
-    #     cam_type = "grad_cam_plus_plus"
-    #     target_layers = [resnet50_plus.layer4[-1]]
-    #     fmeasure = FaithfulnessMeasurment(resnet50, target_layers, ds_test,model_grad_cam_plus_plus=resnet50_plus,cam_type=cam_type)
+        # target layer:
+        cam_type = "grad_cam_plus_plus"
+        target_layers = [resnet50_plus.layer4[-1]]
+        fmeasure = FaithfulnessMeasurment(resnet50, target_layers, ds_test,model_grad_cam_plus_plus=resnet50_plus,cam_type=cam_type)
         
-    #     data = fmeasure.get_all_same_sl_map(tr_fc=threshold_fc)
-    #     data.to_csv(f"finished/ResNet50_new/faithfulness_metrics_grad_cam_plus_plus_combained_noise_{name}.csv",index=False)
+        data = fmeasure.get_all_same_sl_map(tr_fc=threshold_fc)
+        data.to_csv(f"finished/ResNet50_new/faithfulness_metrics_grad_cam_plus_plus_combained_noise_{name}.csv",index=False)
     print("vgg")
     print("inverted")
     for noise,name in zip([0.5,1.0,1.5],["0_5","1","1_5"]):
@@ -175,37 +175,37 @@ if __name__ == '__main__':
         data = fmeasure.get_all_same_sl_map(tr_fc=threshold_fc,perturbation_fc=eurosat_perturbation_inverted)
         data.to_csv(f"finished/VGG16/faithfulness_metrics_grad_cam_inverted_noise_{name}.csv",index=False)
     print("combained")
-    # for noise,name in zip([0.5,1.0,1.5],["0_5","1","1_5"]):
-    #     def threshold_fc(silency_map):
-    #         return thr_fc(silency_map,scale=noise)
-    #     # model load:
-    #     vgg = torch.load("finished\\VGG16\\vgg_model.pth")
-    #     vgg.cuda()
-    #     for param in vgg.features.parameters():
-    #             param.requires_grad = True
-    #     # add softmax
+    for noise,name in zip([0.5,1.0,1.5],["0_5","1","1_5"]):
+        def threshold_fc(silency_map):
+            return thr_fc(silency_map,scale=noise)
+        # model load:
+        vgg = torch.load("finished\\VGG16\\vgg_model.pth")
+        vgg.cuda()
+        for param in vgg.features.parameters():
+                param.requires_grad = True
+        # add softmax
 
-    #     fc_ = list(vgg.classifier)
-    #     fc_.append(torch.nn.Softmax(dim=1))
-    #     vgg.classifier = torch.nn.Sequential(*fc_)
-    #     vgg.eval()
+        fc_ = list(vgg.classifier)
+        fc_.append(torch.nn.Softmax(dim=1))
+        vgg.classifier = torch.nn.Sequential(*fc_)
+        vgg.eval()
 
-    #     # dataset:
-    #     transforms = v2.Compose([
-    #         v2.ToTensor(),
-    #         v2.ToDtype(torch.float32),
-    #         v2.Resize(224,antialias=None),
-    #     ])
-    #     ds = EuroSAT("../EuroSat",transform=transforms,target_transform=transformation_eurosat,download=False)
-    #     ds_test = Test_Dataset_EuroSat(ds)
+        # dataset:
+        transforms = v2.Compose([
+            v2.ToTensor(),
+            v2.ToDtype(torch.float32),
+            v2.Resize(224,antialias=None),
+        ])
+        ds = EuroSAT("../EuroSat",transform=transforms,target_transform=transformation_eurosat,download=False)
+        ds_test = Test_Dataset_EuroSat(ds)
 
-    #     # target layer:
-    #     cam_type = "grad_cam"
-    #     target_layers = [vgg.features[-2]]
-    #     fmeasure = FaithfulnessMeasurment(vgg, target_layers, ds_test)
+        # target layer:
+        cam_type = "grad_cam"
+        target_layers = [vgg.features[-2]]
+        fmeasure = FaithfulnessMeasurment(vgg, target_layers, ds_test)
         
-    #     data = fmeasure.get_all_same_sl_map(tr_fc=threshold_fc)
-    #     data.to_csv(f"finished/VGG16/faithfulness_metrics_grad_cam_combained_noise_{name}.csv",index=False)
+        data = fmeasure.get_all_same_sl_map(tr_fc=threshold_fc)
+        data.to_csv(f"finished/VGG16/faithfulness_metrics_grad_cam_combained_noise_{name}.csv",index=False)
     print("inverted++")
     for noise,name in zip([0.5,1.0,1.5],["0_5","1","1_5"]):
         def threshold_fc(silency_map):
@@ -244,39 +244,39 @@ if __name__ == '__main__':
         data = fmeasure.get_all_same_sl_map(tr_fc=threshold_fc,perturbation_fc=eurosat_perturbation_inverted)
         data.to_csv(f"finished/VGG16/faithfulness_metrics_grad_cam_plus_plus_inverted_noise_{name}.csv",index=False)
     print("combained++")
-    # for noise,name in zip([0.5,1.0,1.5],["0_5","1","1_5"]):
-    #     def threshold_fc(silency_map):
-    #         return thr_fc(silency_map,scale=noise)
-    #     # model load:
-    #     vgg = torch.load("finished\\VGG16\\vgg_model.pth")
-    #     vgg.cuda()
-    #     for param in vgg.features.parameters():
-    #             param.requires_grad = True
-    #     # add softmax
-    #     fc_ = list(vgg.classifier)
-    #     fc_.append(torch.nn.Softmax(dim=1))
-    #     vgg.classifier = torch.nn.Sequential(*fc_)
-    #     vgg.eval()
+    for noise,name in zip([0.5,1.0,1.5],["0_5","1","1_5"]):
+        def threshold_fc(silency_map):
+            return thr_fc(silency_map,scale=noise)
+        # model load:
+        vgg = torch.load("finished\\VGG16\\vgg_model.pth")
+        vgg.cuda()
+        for param in vgg.features.parameters():
+                param.requires_grad = True
+        # add softmax
+        fc_ = list(vgg.classifier)
+        fc_.append(torch.nn.Softmax(dim=1))
+        vgg.classifier = torch.nn.Sequential(*fc_)
+        vgg.eval()
 
-    #     # model for grad cam ++
-    #     vgg_plus = torch.load("finished\\VGG16\\vgg_model.pth")
-    #     vgg_plus.cuda()
-    #     for param in vgg_plus.features.parameters():
-    #             param.requires_grad = True
+        # model for grad cam ++
+        vgg_plus = torch.load("finished\\VGG16\\vgg_model.pth")
+        vgg_plus.cuda()
+        for param in vgg_plus.features.parameters():
+                param.requires_grad = True
         
-    #     # dataset:
-    #     transforms = v2.Compose([
-    #         v2.ToTensor(),
-    #         v2.ToDtype(torch.float32),
-    #         v2.Resize(224,antialias=None),
-    #     ])
-    #     ds = EuroSAT("../EuroSat",transform=transforms,target_transform=transformation_eurosat,download=False)
-    #     ds_test = Test_Dataset_EuroSat(ds)
+        # dataset:
+        transforms = v2.Compose([
+            v2.ToTensor(),
+            v2.ToDtype(torch.float32),
+            v2.Resize(224,antialias=None),
+        ])
+        ds = EuroSAT("../EuroSat",transform=transforms,target_transform=transformation_eurosat,download=False)
+        ds_test = Test_Dataset_EuroSat(ds)
 
-    #     # target layer:
-    #     cam_type = "grad_cam_plus_plus"
-    #     target_layers = [vgg_plus.features[-2]]
-    #     fmeasure = FaithfulnessMeasurment(vgg, target_layers, ds_test,cam_type=cam_type,model_grad_cam_plus_plus=vgg_plus)
+        # target layer:
+        cam_type = "grad_cam_plus_plus"
+        target_layers = [vgg_plus.features[-2]]
+        fmeasure = FaithfulnessMeasurment(vgg, target_layers, ds_test,cam_type=cam_type,model_grad_cam_plus_plus=vgg_plus)
         
-    #     data = fmeasure.get_all_same_sl_map(tr_fc=threshold_fc)
-    #     data.to_csv(f"finished/VGG16/faithfulness_metrics_grad_cam_plus_plus_combained_noise_{name}.csv",index=False)
+        data = fmeasure.get_all_same_sl_map(tr_fc=threshold_fc)
+        data.to_csv(f"finished/VGG16/faithfulness_metrics_grad_cam_plus_plus_combained_noise_{name}.csv",index=False)
